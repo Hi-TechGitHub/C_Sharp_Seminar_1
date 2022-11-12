9cf48959d1ff78ea8526ae58f7c571660c1602fd
Hi-TechGitHub <116220279+Hi-TechGitHub@users.noreply.github.com>
2022-11-12 14:04:01 +0300

    task00

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Впишите 3 числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max;
+if (number1 > number2) {
+    max = number1;
+    if (number1 > number3) {
+        System.Console.WriteLine("Самое большое число: ");
+        System.Console.WriteLine(max);
+        
+    }
+}
+
+
+if (number1 < number2) {
+    max = number2;
+    if (number2 > number3) {
+        System.Console.WriteLine("Самое большое число: ");
+        System.Console.WriteLine(max);
+        
+    }
+}
+
+if (number3 > number1) {
+    max = number3;
+    if (number2 < number3) {
+        System.Console.WriteLine("Самое большое число: ");
+        System.Console.WriteLine(max);
+
+        
+    }
+}
