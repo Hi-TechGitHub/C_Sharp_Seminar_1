4ca0b9faa992794dcc90d3d5f5f29aa3c84ec36d
Hi-TechGitHub <116220279+Hi-TechGitHub@users.noreply.github.com>
2022-11-12 15:10:02 +0300

    task03

---
+++ task03/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("Впишите число.");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int start = 0;
+while (start < number1) {
+    start += 2;
+    System.Console.WriteLine(start);
+}
