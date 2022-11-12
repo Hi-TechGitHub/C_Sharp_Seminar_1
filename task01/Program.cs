5205b50751c1bd0c69afd636814203dc0b9dfe0d
Hi-TechGitHub <116220279+Hi-TechGitHub@users.noreply.github.com>
2022-11-12 14:38:31 +0300

    task01

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Впишите 2 числа");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max;
+if (number1 > number2) {
+    max = number1;
+}
+else {
+    max = number2;
+}
+Console.WriteLine("Самое большое число это: " + max);
