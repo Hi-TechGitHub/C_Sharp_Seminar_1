86e232b4a8eea97003b69627fd93e6303ff0f14d
Hi-TechGitHub <116220279+Hi-TechGitHub@users.noreply.github.com>
2022-11-12 15:00:45 +0300

    task02

---
+++ task02/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Впишите число и проверим, чётное ли оно");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 % 2 == 0) {
+    
+    System.Console.WriteLine("Число четное");
+    }
+    
+
+
+else {
+    System.Console.WriteLine("Число не четное");
+}
